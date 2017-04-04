5f80b6b07f00b403e6621486b2d975637aae6392
Chentte <isc.vperea@gmail.com>
2017-04-04 00:40:34 -0500

    modulo3

---
+++ Modulo3/Semana1/Ejercicio1/Ejercico1_project/app/src/main/java/com/android/curso/chente/ejercico1/MainActivity.java
@@ -0,0 +1,13 @@
+package com.android.curso.chente.ejercico1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
